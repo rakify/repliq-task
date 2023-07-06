674412d8f605332822d8d58cd9c5b43c4e18b421
rakify <irakibm@gmail.com>
2023-07-07 00:14:09 +0600

    feat: add authProvider and toastProvider

---
+++ src/app/providers/auth.provider.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { UserContext, defaultCart } from "@/context/userContext";
+import { Props } from "@/interfaces/default";
+import { IUser } from "@/interfaces/user";
+import { useEffect, useState } from "react";
+
+export default function AuthProvider({ children }: Props) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+  const [cart, setCart] = useState(defaultCart);
+
+  const logoutUser = () => {
+    setCurrentUser(null);
+    setCart(defaultCart);
+    localStorage.clear();
+  };
+
+  useEffect(() => {
+    const userDataString = localStorage.getItem("currentUser");
+
+    if (userDataString) {
+      try {
+        const userData: IUser = JSON.parse(userDataString);
+        setCurrentUser(userData);
+        setIsFetching(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    } else {
+      setIsFetching(false);
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isFetching, isError, cart, setCart, logoutUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}

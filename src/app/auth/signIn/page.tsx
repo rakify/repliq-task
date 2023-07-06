613cf954d3b1b5a0843c12fc61d47029fd9974cb
rakify <irakibm@gmail.com>
2023-07-06 21:59:20 +0600

    feat: add sign in page

---
+++ src/app/auth/signIn/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Container from "@/components/container";
+import SignInForm from "@/components/forms/signIn/signin-form";
+
+export default function SignIn() {
+  return (
+    <Container>
+      <SignInForm />
+    </Container>
+  );
+}

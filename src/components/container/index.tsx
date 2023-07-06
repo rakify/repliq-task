7e99243287f674aa07ea9c4e70318cf2c885c583
rakify <irakibm@gmail.com>
2023-07-06 21:57:24 +0600

    feat: add container component

---
+++ src/components/container/index.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import { IContainer } from "./interface";
+import Styles from "./container.module.css";
+
+export default function Container({
+  children,
+  isFluid = false,
+  isNoPadding = false,
+}: IContainer) {
+  const containerClasses = clsx(
+    [Styles["container"]],
+    { [Styles["full-width"]]: isFluid },
+    { [Styles["no-padding"]]: isNoPadding }
+  );
+  return (
+    <div role="presentation" className={containerClasses}>
+      {children}
+    </div>
+  );
+}

b9f8310ae9248dc48f4bacf507959d007b7a87fe
rakify <irakibm@gmail.com>
2023-07-09 00:57:03 +0600

    feat: customize mui data grid toolbar

---
+++ src/utils/quick-search-toolbar.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material';
+import { GridToolbar } from '@mui/x-data-grid';
+
+export default function QuickSearchToolbar() {
+  return (
+    <Box
+      sx={{
+        p: 0.5,
+        pb: 0,
+      }}
+    >
+      <GridToolbar
+        quickFilterParser={(searchInput: string) =>
+          searchInput.split(',').map((value) => value.trim())
+        }
+        quickFilterFormatter={(quickFilterValues: any[]) =>
+          quickFilterValues.join(', ')
+        }
+        debounceMs={200} // time before applying the new quick filter value
+      />
+    </Box>
+  );
+}

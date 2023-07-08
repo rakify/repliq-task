4431dac95ed425faec53ac48779cd91c81ad6556
rakify <irakibm@gmail.com>
2023-07-08 22:57:21 +0600

    chore: cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image';
 import Products from './products/page';
-import Footer from '@/components/layouts/footer';
-import Navbar from '@/components/layouts/navbar';
 
 export default function Home() {
   return (

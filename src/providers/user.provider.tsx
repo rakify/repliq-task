4a09f3e142645cfa269d3f0f2210903cdff5a2a6
rakify <irakibm@gmail.com>
2023-07-08 18:52:08 +0600

    wip: cart

--- src/providers/user.provider.tsx
+++ src/providers/user.provider.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { getCartProducts } from '@/context/apiCalls';
 import { UserContext, defaultCart } from '@/context/userContext';
+import { ICart, ICartSuccessResponse } from '@/interfaces/cart.interface';
 import { Props } from '@/interfaces/default.interface';
 import { IUser } from '@/interfaces/user.interface';
 import { useEffect, useState } from 'react';
@@ -17,13 +19,39 @@ export default function UserProvider({ children }: Props) {
     localStorage.clear();
   };
 
+  const fetchCartProducts = async (id: string) => {
+    setIsFetching(true);
+    try {
+      const response: ICartSuccessResponse = await getCartProducts(id);
+
+      if (response.status && response.status === 200) {
+        const cart: ICart = {
+          products: response.data.products,
+          total: response.data.total,
+          isError: false,
+          isFetching: false,
+        };
+        localStorage.setItem('cart', JSON.stringify(cart));
+      }
+      setIsFetching(false);
+    } catch (error) {
+      setIsError(true);
+      setIsFetching(false);
+    }
+  };
+
+  // setup user
   useEffect(() => {
     const userDataString = localStorage.getItem('currentUser');
+    const cartDataString = localStorage.getItem('currentUser');
 
     if (userDataString) {
       try {
         const userData: IUser = JSON.parse(userDataString);
         setCurrentUser(userData);
+        //fetch cart from api if it is not available locally
+        if (!cartDataString) fetchCartProducts(userData._id);
+        if (cartDataString) setCart(JSON.parse(cartDataString));
         setIsFetching(false);
       } catch (error) {
         setIsError(true);
@@ -35,7 +63,18 @@ export default function UserProvider({ children }: Props) {
 
   return (
     <UserContext.Provider
-      value={{ currentUser, isFetching, isError, cart, setCart, logoutUser }}
+      value={{
+        currentUser,
+        setCurrentUser,
+        isFetching,
+        setIsFetching,
+        isError,
+        setIsError,
+        cart,
+        setCart,
+        logoutUser,
+        fetchCartProducts,
+      }}
     >
       {children}
     </UserContext.Provider>
